d4b66a3dd836856bbed174e98e7016a922e5392b
Qangan <Qangan@users.noreply.github.com>
2025-09-08 18:10:35 +0000

    Deploying to gh-pages from @ Qangan/OOP@86dcf4c295f8e4c6af100324efc27195bbc90098 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"qangan.heapsort","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"qangan.heapsort","l":"Heapsort"},{"p":"qangan.heapsort","l":"Main"}];updateSearchResults();
