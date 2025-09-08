7b05e6e434164c9aa3a8869874d74f7f684bfb29
Qangan <danilfulmanf@gmail.com>
2025-09-08 19:59:50 +0700

    reviewdog att2

--- Task_1_1_1/app/src/main/java/qangan/heapsort/Main.java
+++ Task_1_1_1/app/src/main/java/qangan/heapsort/Main.java
@@ -2,20 +2,25 @@ package qangan.heapsort;
 
 import java.util.Arrays;
 
+/**
+ * HeapSort implementation class.
+ */
+
 public class Main {
     /**
-     * Supports heap invariance
+     * Supports heap invariance.
      */
+
     private static void heapify(int arr[], int n, int i){
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
         
-        if (left < n && arr[left] > arr[largest]){
+        if (left < n && arr[left] > arr[largest]) {
             largest = left;
         }
 
-        if (right < n && arr[right] > arr[largest]){
+        if (right < n && arr[right] > arr[largest]) {
             largest = right;
         }
         if (largest != i){
@@ -27,8 +32,9 @@ public class Main {
         }
     }
     /**
-     *  Builds heap
+     *  Builds heap.
      */
+
     private static void makeHeap(int[] arr) {
         int n = arr.length;
         for (int i = n / 2 - 1; i >= 0; i--) {
@@ -40,10 +46,11 @@ public class Main {
      *
      * @param arr int - array to sort
      */
+
     public static void heapSort(int[] arr) {
         makeHeap(arr);
         int n = arr.length;
-        for(int i = n - 1; i >= 0; i--){
+        for(int i = n - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
@@ -52,8 +59,9 @@ public class Main {
         }
     }
     /** 
-     * Example usage
+     * Example usage.
      */
+
     public static void main(String[] args) {
         int[] a = {1,2,9,8,7,6,99};
         heapSort(a);
