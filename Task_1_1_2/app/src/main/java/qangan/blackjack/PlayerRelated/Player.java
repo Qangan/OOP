86dec46236cae45cba7437e90b1e0f0ccc84417b
Qangan <danilfulmanf@gmail.com>
2025-09-16 20:07:04 +0700

    task 1-1-2 no dealer and gamelogic

---
+++ Task_1_1_2/app/src/main/java/qangan/blackjack/PlayerRelated/Player.java
@@ -0,0 +1,51 @@
+package qangan.blackjack;
+
+/*
+ * Player class description.
+ */
+public class Player {
+    private Hand hand;
+
+    /*
+     * Player constructor.
+     */
+    public Player() {
+        this.hand = new Hand();
+    }
+
+    /*
+     * Draw card.
+     */
+    public void receive(Deck deck) {
+        hand.addCard(deck);
+    }
+
+    /*
+     * Blackjack checker.
+     */
+    public boolean hasBlackjack() { 
+        return hand.isBlackjack();
+    }
+    
+    /*
+     * Hand score getter.
+     */
+    public int getScore() { 
+        return hand.getScore(); 
+    }
+
+    /*
+     * Hand getter.
+     */
+    public Hand getHand() { 
+        return hand; 
+    }
+
+    /*
+     * Prints hand.
+     */
+    public String showHand() {
+        return hand.toString() + " => " + hand.getScore();
+    }
+}
+
